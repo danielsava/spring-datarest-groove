b2bfe710097cc5821537c1495fdd9a65488b550b
Daniel Sava <danielsavas@gmail.com>
2022-05-16 13:58:57 -0300

    Primeiro commit

---
+++ src/main/groovy/com/example/springdatarestgroove/SpringDatarestGrooveApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.springdatarestgroove
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringDatarestGrooveApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringDatarestGrooveApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/example/springdatarestgroove/SpringDatarestGrooveApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.springdatarestgroove
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDatarestGrooveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
