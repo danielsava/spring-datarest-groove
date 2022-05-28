8eaa2bd975001839b66d4faae51f08a4bc50eeb6
Daniel Sava <danielsavas@gmail.com>
2022-05-28 16:22:35 -0300

    Configurações de CORS

---
+++ src/main/groovy/app/config/Config.groovy
@@ -0,0 +1,17 @@
+package app.config
+
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer
+import org.springframework.stereotype.Component
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+@Component
+class Config implements RepositoryRestConfigurer {
+
+
+    @Override
+    void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        cors.addMapping("/**")
+    }
+}
--- src/main/groovy/app/repository/FuncionalidadeRepository.groovy
+++ src/main/groovy/app/repository/FuncionalidadeRepository.groovy
@@ -3,9 +3,7 @@ package app.repository
 import app.model.Funcionalidade
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
-import org.springframework.web.bind.annotation.CrossOrigin
 
-@CrossOrigin(origins = "*")
 @RepositoryRestResource(collectionResourceRel = "funcionalidades", path = "funcionalidade")
 interface FuncionalidadeRepository extends PagingAndSortingRepository<Funcionalidade, Long> {
 
