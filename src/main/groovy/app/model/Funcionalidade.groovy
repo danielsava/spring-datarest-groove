1e7238479b04d10ebad6e6772d7cff088dceee80
Daniel Sava <danielsavas@gmail.com>
2022-05-30 23:53:34 -0300

    Bean validations

--- src/main/groovy/app/model/Funcionalidade.groovy
+++ src/main/groovy/app/model/Funcionalidade.groovy
@@ -4,13 +4,16 @@ import estrutura.model.jpa.EntidadeJPA
 import groovy.transform.builder.Builder
 
 import javax.persistence.Entity
+import javax.validation.constraints.NotEmpty
 
 @Entity @Builder(builderMethodName = "of")
 class Funcionalidade extends EntidadeJPA {
 
 
+    @NotEmpty(message = "Informe o nome")
     String nome
 
+    @NotEmpty(message = "Informe a descrição")
     String descricao
 
 
