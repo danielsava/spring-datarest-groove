9a4913f2e15c6327a8c0805d5bd4a0d826af3ef2
Daniel Sava <danielsavas@gmail.com>
2022-06-01 20:28:44 -0300

    Bean validations

--- src/main/groovy/app/model/Funcionalidade.groovy
+++ src/main/groovy/app/model/Funcionalidade.groovy
@@ -1,14 +1,13 @@
 package app.model
 
-
 import app.model.validation.SecondOrder
 import estrutura.model.jpa.EntidadeJPA
 import groovy.transform.builder.Builder
 
 import javax.persistence.Entity
 import javax.validation.GroupSequence
-import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 
 @Entity @Builder(builderMethodName = "of")
 @GroupSequence([ Funcionalidade, SecondOrder ])
@@ -18,7 +17,7 @@ class Funcionalidade extends EntidadeJPA {
     @NotBlank(message = "Informe o nome")
     String nome
 
-    @Min(value = 2L, message = "Descrição muito curta", groups = SecondOrder)
+    @Size(min = 4, message = "Descrição muito curta", groups = SecondOrder)
     @NotBlank(message = "Informe a descrição")
     String descricao
 
